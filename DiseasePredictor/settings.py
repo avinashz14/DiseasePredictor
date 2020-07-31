a6bb8ccf502485eb2a49fb03213930d94421a15b
avinashz14 <prajapati143a@gmail.com>
2020-08-01 00:51:31 +0530

    updates domain

--- DiseasePredictor/settings.py
+++ DiseasePredictor/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nu9s)f+a$47w-%bv6-6gq44qek!usu$$fgn5vjpz2t98s2#_zc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://checkkup.tk']
 
 
 # Application definition
